9faf53909d881690120eb8383236e308957dad23
于高衡 <18661139072@163.com>
2022-11-22 09:21:06 +0800

    init

---
+++ src/views/dashboard/workplace/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+    <div class="left-side">
+      <div class="panel">
+        <Banner />
+        <Statistics />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import Banner from './components/banner.vue';
+  import Statistics from './components/statistics.vue';
+</script>
+
+<script lang="ts">
+  export default {
+    name: 'Dashboard',
+  };
+</script>
+
+<style lang="less" scoped>
+  .container {
+    display: flex;
+    padding: 16px 20px;
+    padding-bottom: 0;
+    background-color: var(--color-fill-2);
+  }
+
+  .left-side {
+    flex: 1;
+    overflow: auto;
+  }
+
+  .panel {
+    overflow: auto;
+    background-color: var(--color-bg-2);
+    border-radius: 4px;
+  }
+
+  :deep(.panel-border) {
+    margin-bottom: 0;
+    border-bottom: 1px solid rgb(var(--gray-2));
+  }
+
+  .moduler-wrap {
+    background-color: var(--color-bg-2);
+    border-radius: 4px;
+
+    :deep(.text) {
+      color: rgb(var(--gray-8));
+      font-size: 12px;
+      text-align: center;
+    }
+
+    :deep(.wrapper) {
+      margin-bottom: 8px;
+      text-align: center;
+      cursor: pointer;
+
+      &:last-child {
+        .text {
+          margin-bottom: 0;
+        }
+      }
+
+      &:hover {
+        .icon {
+          color: rgb(var(--arcoblue-6));
+          background-color: #e8f3ff;
+        }
+
+        .text {
+          color: rgb(var(--arcoblue-6));
+        }
+      }
+    }
+
+    :deep(.icon) {
+      display: inline-block;
+      width: 32px;
+      height: 32px;
+      margin-bottom: 4px;
+      color: rgb(var(--dark-gray-1));
+      font-size: 16px;
+      line-height: 32px;
+      text-align: center;
+      background-color: rgb(var(--gray-1));
+      border-radius: 4px;
+    }
+  }
+</style>
+
+<style lang="less" scoped>
+  // responsive
+  .mobile {
+    .container {
+      display: block;
+    }
+  }
+</style>
